516032af53f7afbd9c12fdeca966314c10d895cf
sivarajesh767 <sivarajesh767@gmail.com>
2024-10-17 09:07:14 +0000

    Output result is zero score but login and logout button is correctly exicute

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,36 +1,17 @@
-import './index.css'
 import Cookies from 'js-cookie'
 import {Redirect, withRouter} from 'react-router-dom'
+import './index.css'
 
 const Login = props => {
-  const jwtToken = Cookies.get('jwt_token')
-  const setCookiesAndNavigativeToHome = token => {
+  const onClickButton = () => {
     const {history} = props
-    Cookies.set('jwt_token', token, {expire: 30})
+    Cookies.remove('jwt_token')
     history.replace('/')
-  }
-  const onClickLogin = async () => {
-    const userDetails = {username: 'siva', password: 'siva@123'}
-    const url = 'https://apis.ccbp.in/login'
-    const options = {
-      method: 'POST',
-      body: JSON.strigify(userDetails),
-    }
-    const response = await fetch(url, options)
-    const data = await response.json()
-    if (response.ok === true) {
-      setCookiesAndNavigativeToHome(data.jwt_token)
-    }
-  }
-
-  if (jwtToken !== undefined) {
-    return <Redirect to="/" />
   }
   return (
     <div>
-      <h1>Place Login</h1>
-      <button type="button" onClick={onClickLogin}>
-        Login With sample credit
+      <button type="button" onClick={onClickButton}>
+        Login
       </button>
     </div>
   )
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,8 +2,8 @@
 import './index.css'
 
 const NotFound = () => (
-  <div>
+  <>
     <h1>Not Found</h1>
-  </div>
+  </>
 )
 export default NotFound
